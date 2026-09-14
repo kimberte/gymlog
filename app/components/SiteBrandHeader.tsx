610953ed02cf2cadd1764e14511ffd2f19201b3d
Kimberte William <kimberte@gmail.com>
2026-09-14 16:15:16 -0400

    Use shared Gym Log header on launcher and welcome pages

--- app/components/SiteBrandHeader.tsx
+++ app/components/SiteBrandHeader.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const HIDDEN_PATHS = new Set(["/", "/launcher", "/welcome", "/subscribe"]);
+const HIDDEN_PATHS = new Set(["/", "/subscribe"]);
 
 export default function SiteBrandHeader() {
   const pathname = usePathname();

d774e8c758133c9584d3d7ea851a04327f886697
Kimberte William <kimberte@gmail.com>
2026-09-17 08:13:11 -0400

    Wire new Gym Log blog posts into blog index

--- app/blog/blogPosts.ts
+++ app/blog/blogPosts.ts
@@ -1,8 +1,9 @@
 import { BLOG_POSTS, getBlogPost as getOriginalBlogPost } from "./blogData";
 import { ADDITIONAL_BLOG_POSTS } from "./additionalPosts";
+import { NEW_BLOG_POSTS } from "./newPosts";
 
-export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS];
+export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS, ...NEW_BLOG_POSTS];
 
 export function getBlogPost(slug: string) {
-  return getOriginalBlogPost(slug) ?? ADDITIONAL_BLOG_POSTS.find(post => post.slug === slug);
+  return getOriginalBlogPost(slug) ?? ADDITIONAL_BLOG_POSTS.find(post => post.slug === slug) ?? NEW_BLOG_POSTS.find(post => post.slug === slug);
 }

18e77d93fa82205559ddd1381f40307391cd2389
Kimberte William <kimberte@gmail.com>
2026-09-20 14:55:30 -0400

    Include September 20 Gym Log blog posts

--- app/blog/blogPosts.ts
+++ app/blog/blogPosts.ts
@@ -2,8 +2,9 @@ import { BLOG_POSTS, getBlogPost as getOriginalBlogPost } from "./blogData";
 import { ADDITIONAL_BLOG_POSTS } from "./additionalPosts";
 import { NEW_BLOG_POSTS } from "./newPosts";
 import { DAILY_BLOG_POSTS } from "./dailyPosts";
+import { DAILY_BLOG_POSTS_2 } from "./dailyPosts2";
 
-export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS, ...NEW_BLOG_POSTS, ...DAILY_BLOG_POSTS].sort(
+export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS, ...NEW_BLOG_POSTS, ...DAILY_BLOG_POSTS, ...DAILY_BLOG_POSTS_2].sort(
   (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
 );
 
@@ -11,5 +12,6 @@ export function getBlogPost(slug: string) {
   return getOriginalBlogPost(slug)
     ?? ADDITIONAL_BLOG_POSTS.find(post => post.slug === slug)
     ?? NEW_BLOG_POSTS.find(post => post.slug === slug)
-    ?? DAILY_BLOG_POSTS.find(post => post.slug === slug);
+    ?? DAILY_BLOG_POSTS.find(post => post.slug === slug)
+    ?? DAILY_BLOG_POSTS_2.find(post => post.slug === slug);
 }

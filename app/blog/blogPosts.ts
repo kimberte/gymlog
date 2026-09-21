2ca9d359d6e5ceea40e551f49b2aa0ff7347292f
Kimberte William <kimberte@gmail.com>
2026-09-21 15:49:43 -0400

    Wire September 21 Gym Log posts into blog index

--- app/blog/blogPosts.ts
+++ app/blog/blogPosts.ts
@@ -3,8 +3,9 @@ import { ADDITIONAL_BLOG_POSTS } from "./additionalPosts";
 import { NEW_BLOG_POSTS } from "./newPosts";
 import { DAILY_BLOG_POSTS } from "./dailyPosts";
 import { DAILY_BLOG_POSTS_2 } from "./dailyPosts2";
+import { DAILY_BLOG_POSTS_3 } from "./dailyPosts3";
 
-export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS, ...NEW_BLOG_POSTS, ...DAILY_BLOG_POSTS, ...DAILY_BLOG_POSTS_2].sort(
+export const ALL_BLOG_POSTS = [...BLOG_POSTS, ...ADDITIONAL_BLOG_POSTS, ...NEW_BLOG_POSTS, ...DAILY_BLOG_POSTS, ...DAILY_BLOG_POSTS_2, ...DAILY_BLOG_POSTS_3].sort(
   (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
 );
 
@@ -13,5 +14,6 @@ export function getBlogPost(slug: string) {
     ?? ADDITIONAL_BLOG_POSTS.find(post => post.slug === slug)
     ?? NEW_BLOG_POSTS.find(post => post.slug === slug)
     ?? DAILY_BLOG_POSTS.find(post => post.slug === slug)
-    ?? DAILY_BLOG_POSTS_2.find(post => post.slug === slug);
+    ?? DAILY_BLOG_POSTS_2.find(post => post.slug === slug)
+    ?? DAILY_BLOG_POSTS_3.find(post => post.slug === slug);
 }

dbfca6edbbf9a86f8a7057f281ad87dee234bd05
Kimberte William <kimberte@gmail.com>
2026-09-24 09:31:56 -0400

    Add Gym Log tools hub route

---
+++ app/tools/page.tsx
@@ -0,0 +1,3 @@
+import { ToolsHub } from "./ToolsClient";
+export const metadata={title:"Gym Tools | Gym Log",description:"Free gym calculators, timers and training tools from Gym Log."};
+export default function ToolsPage(){return <ToolsHub/>}
